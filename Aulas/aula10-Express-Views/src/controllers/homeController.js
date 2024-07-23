2c46e5299f67fd13d7ea6c68eb9e176352a185b0
Luís André <andre2016999@gmail.com>
2024-07-23 17:02:58 -0300

    Express Views

---
+++ Aulas/aula10-Express-Views/src/controllers/homeController.js
@@ -0,0 +1,13 @@
+exports.paginaInicial = (req, res) => {
+    res.render('index')
+    // res.send(`
+    //           <form action="/" method="POST">
+    //             Nome do cliente = <input type="text" name="Nome">
+    //             <button>AOOOBAAAA BAOOOOO</button>
+    //             </form>    
+    //         `)
+}
+
+exports.trataPost = (req, res) => {
+    res.send('BAOOOOOOO')
+}
