a6ee6eac52882dd228311ca6239c42e41c285f1a
Luís André <andre2016999@gmail.com>
2024-07-23 17:04:36 -0300

    Express Webpack

---
+++ Aulas/aula12-Express-Webpack/serve.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+//          criar    ler  atualizar  deletar
+//  CRUD -> CREATE, READ,   UPDATE,  DELETE
+//           post    get     put     delete
+
+// http://meusite.com/ <- GET -> entrega da pagina
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use(routes)
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+
+app.listen(3001, () => {
+    console.log('Acessar http://localhost:3001')
+    console.log('servidor rodando na porta 3001')
+})
